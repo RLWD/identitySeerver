8e1fb2ee8c1177b280f5385efb453857f23dd88d
RLWD <richard.lawidjaja@vandewiele.com>
2021-11-11 21:40:34 +0800

    Adding functionalities to support blob

---
+++ VDW.SalesApp.Common/BlobStorage/BlobProvider.cs
@@ -0,0 +1,26 @@
+﻿using Azure.Storage.Blobs;
+using System;
+
+namespace VDW.SalesApp.Common.BlobStorage
+{
+    public class BlobProvider
+    {
+        public BlobProvider() { }
+        public BlobContainerClient GetBlobContainerClient(string connectionString, string containerName)
+        {
+            if (string.IsNullOrEmpty(connectionString) || string.IsNullOrEmpty(containerName))
+                throw new ArgumentException("invalid argument, check your settings");
+            try
+            {
+                var serviceClient = new BlobServiceClient(connectionString);
+                if (!serviceClient.GetBlobContainerClient(containerName).Exists())
+                    throw new ArgumentException("container name does not exist");
+                return serviceClient.GetBlobContainerClient(containerName);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
