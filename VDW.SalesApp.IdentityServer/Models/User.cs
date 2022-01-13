358b361ad532e2f161d34a0b6cf507211585ccc0
RLWD <richard.lawidjaja@vandewiele.com>
2022-01-14 04:12:50 +0800

    Custom Claim, Validation and Login History

--- VDW.SalesApp.IdentityServer/Models/User.cs
+++ VDW.SalesApp.IdentityServer/Models/User.cs
@@ -7,5 +7,6 @@ namespace VDW.SalesApp.IdentityServer.Models
         public string UserName { get; set; }
         public string Password { get; set; }
         public bool IsActive { get; set; }
+        public int UserId { get; set; }
     }
 }
--- VDW.SalesApp.IdentityServer/Repository/Interface/IUserRepository.cs
+++ VDW.SalesApp.IdentityServer/Repository/Interface/IUserRepository.cs
@@ -6,8 +6,9 @@ namespace VDW.SalesApp.IdentityServer.Repository.Interface
 {
     public interface IUserRepository
     {
-        public Task<UserClaims> GetUserByOtp(string phoneNumber, string tokenValue);
+        public Task<UserClaims> GetUserByOtp(string phoneNumber, string tokenValue, int tokenExpirySeconds);
         public Task<UserClaims> GetUserByPassword(string phoneNumber);
         public Task<User> GetPasswordByUserName(string userName);
+        void LogFailedAttempt(string userName, string failReason);
     }
 }
