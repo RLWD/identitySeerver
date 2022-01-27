c7d5d76258b499fafc8826459fb0df5af7698d86
Richard Lawidjaja <richard.lawidjaja@vandewiele.com>
2022-01-27 19:06:30 +0800

    Refactor Auth code

--- VDW.SalesApp.IdentityServer/Services/ProfileServices.cs
+++ VDW.SalesApp.IdentityServer/Services/ProfileServices.cs
@@ -9,59 +9,25 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using VDW.SalesApp.IdentityServer.Models;
 using VDW.SalesApp.IdentityServer.Models.Enum;
-using VDW.SalesApp.IdentityServer.Repository.Interface;
 
 namespace VDW.SalesApp.IdentityServer.Services
 {
     public class ProfileServices : IProfileService
     {
-        private IUserRepository _userRepository;
-        public ProfileServices(IUserRepository userRepository)
-        {
-            _userRepository = userRepository;
-        }
         public async Task GetProfileDataAsync(ProfileDataRequestContext context)
         {
-            AuthenticationRequest request = GetAuthRequest(context.ValidatedRequest.Raw);
-            UserClaims claim;
-            if (request.WorkFlow == Workflow.OTP_LOGIN)
-                claim = await _userRepository.GetUserByOtp(request.Username, request.OtpValue, 180);
-            else
-                claim = await _userRepository.GetUserByPassword(request.Username);
-
-            if (claim != null)
-            {
-                List<Claim> claimList = new List<Claim>();
-                claimList.Add(new Claim("PhoneNumber", claim.PhoneNumber));
-                claimList.Add(new Claim("UserId", claim.UserId));
-                claimList.Add(new Claim("RoleName", claim.Name));
-                context.IssuedClaims = claimList;
-            }
-        }
-
-        private AuthenticationRequest GetAuthRequest(NameValueCollection parameters)
-        {
-            return new AuthenticationRequest
-            {
-                OtpValue = parameters["otp"],
-                Username = parameters["username"],
-                Password = parameters["password"],
-                WorkFlow = parameters["workflow"] == "OTP_LOGIN" ? Workflow.OTP_LOGIN : Workflow.PASSWORD_LOGIN
-            };
+            List<Claim> claimList = new List<Claim>();
+            claimList.Add(new Claim("PhoneNumber", context.ValidatedRequest.Raw["PhoneNumber"]));
+            claimList.Add(new Claim("UserId", context.ValidatedRequest.Raw["UserId"]));
+            claimList.Add(new Claim("RoleName", context.ValidatedRequest.Raw["RoleName"]));
+            claimList.Add(new Claim("RoleName", context.ValidatedRequest.Raw["IsActive"]));
+            context.IssuedClaims = claimList;
         }
 
         public async Task IsActiveAsync(IsActiveContext context)
         {
-            var username = context.Subject.FindFirst("sub").Value;
-            var user = await _userRepository.GetPasswordByUserName(username);
-            if (user != null)
-            {
-                context.IsActive = user.IsActive;
-            }
-            else
-            {
-                context.IsActive = false;
-            }
+            var isActive = context.Subject.FindFirst("IsActive").Value;
+            context.IsActive = isActive.ToLowerInvariant()=="true";
         }
     }
 }
--- VDW.SalesApp.IdentityServer/Services/ResourceOwnerServices.cs
+++ VDW.SalesApp.IdentityServer/Services/ResourceOwnerServices.cs
@@ -1,92 +1,32 @@
 ﻿using IdentityModel;
 using IdentityServer4.Validation;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using VDW.SalesApp.IdentityServer.Models;
 using VDW.SalesApp.IdentityServer.Models.Enum;
-using VDW.SalesApp.IdentityServer.Provider;
-using VDW.SalesApp.IdentityServer.Repository.Interface;
 
 namespace VDW.SalesApp.IdentityServer.Services
 {
     public class ResourceOwnerServices : IResourceOwnerPasswordValidator
     {
-        private readonly IUserRepository _userRepository;
-        public ResourceOwnerServices(IUserRepository userRepository)
-        {
-            _userRepository = userRepository;
-        }
         public async Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
         {
-            AuthenticationRequest request = GetAuthRequest(context.Request.Raw);
-            UserClaims claim;
-            if (request.WorkFlow == Workflow.OTP_LOGIN)
+            List<Claim> claimList = new List<Claim>();
+            claimList.Add(new Claim("PhoneNumber", context.Request.Raw["PhoneNumber"]));
+            claimList.Add(new Claim("UserId", context.Request.Raw["UserId"]));
+            claimList.Add(new Claim("RoleName", context.Request.Raw["RoleName"]));
+            claimList.Add(new Claim("IsActive", context.Request.Raw["IsActive"]));
+
+            if (context.Request.Raw["Otp"] == Workflow.OTP_LOGIN.ToString())
             {
-                claim = await _userRepository.GetUserByOtp(request.Username, request.OtpValue, 180);
-                if (claim == null)
-                {
-                    _userRepository.LogFailedAttempt(request.Username, "Expired OTP");
-                    context.Result = new GrantValidationResult { IsError = true, Error = "INVALID_OTP", ErrorDescription = "Expired OTP" };
-                }
-                else
-                {
-                    List<Claim> claimList = new List<Claim>();
-                    claimList.Add(new Claim("PhoneNumber", claim.PhoneNumber));
-                    claimList.Add(new Claim("UserId", claim.UserId));
-                    claimList.Add(new Claim("RoleName", claim.Name));
-                    context.Result = new GrantValidationResult(claim.PhoneNumber, OidcConstants.GrantTypes.Password, claimList, "local", new Dictionary<string, object>() { { "ForceChangeRequired", true } });
-                }
+  
+                context.Result = new GrantValidationResult(context.Request.Raw["PhoneNumber"], OidcConstants.GrantTypes.Password, claimList, "local", new Dictionary<string, object>() { { "ForceChangeRequired", true } });
             }
             else
             {
-                User user = await _userRepository.GetPasswordByUserName(context.UserName);
-
-                if (user == null || user.UserName != context.UserName)
-                {
-                    _userRepository.LogFailedAttempt(context.UserName, "User Does not exist");
-                    context.Result = new GrantValidationResult { IsError = true, Error = "NO_USER", ErrorDescription = "User Does not exist" };
-                }
-                else
-                {
-                    if (PasswordProvider.Verify(user, context.Password))
-                    {
-                        if (!user.IsActive)
-                        {
-                            _userRepository.LogFailedAttempt(context.UserName, "Deactivated User or User is not Activated");
-                            context.Result = new GrantValidationResult { IsError = true, Error = "NOT_ACTIVE", ErrorDescription = "Deactivated User or User is not Activated" };
-                        }
-                        else
-                        {
-                            claim = await _userRepository.GetUserByPassword(context.UserName);
-                            List<Claim> claimList = new List<Claim>();
-                            claimList.Add(new Claim("PhoneNumber", claim.PhoneNumber));
-                            claimList.Add(new Claim("UserId", claim.UserId));
-                            claimList.Add(new Claim("RoleName", claim.Name));
-
-                            context.Result = new GrantValidationResult(claim.PhoneNumber, OidcConstants.GrantTypes.Password, claimList, "local", new Dictionary<string, object>() { { "ForceChangeRequired", false } });
-                        }
-                    }
-                    else
-                    {
-                        _userRepository.LogFailedAttempt(context.UserName, "Wrong Password");
-                        context.Result = new GrantValidationResult { IsError = true, Error = "INVALID_CREDENTIAL", ErrorDescription = "Unable to Verify the credential" };
-                    }
-                }
+                context.Result = new GrantValidationResult(context.Request.Raw["PhoneNumber"], OidcConstants.GrantTypes.Password, claimList, "local", new Dictionary<string, object>() { { "ForceChangeRequired", false } });
             }
 
-        }
-
-        private AuthenticationRequest GetAuthRequest(NameValueCollection parameters)
-        {
-            return new AuthenticationRequest
-            {
-                OtpValue = parameters["otp"],
-                Username = parameters["username"],
-                Password = parameters["password"],
-                WorkFlow = parameters["workflow"] == "OTP_LOGIN" ? Workflow.OTP_LOGIN : Workflow.PASSWORD_LOGIN
-            };
         }
     }
 }
