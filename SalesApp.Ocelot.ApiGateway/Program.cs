319020effeb4d71f45e7bbce93ebd537cfe45a3c
Xin Yuin Phua <xinyuin.phua@vandewiele.com>
2022-03-02 17:01:24 +0800

    Add ocelot configuration

--- SalesApp.Ocelot.ApiGateway/Program.cs
+++ SalesApp.Ocelot.ApiGateway/Program.cs
@@ -1,29 +1,31 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
 using System.IO;
 
 namespace SalesApp.Ocelot.ApiGateway
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateHostBuilder(args).Build().Run();
+		}
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>().UseContentRoot(Directory.GetCurrentDirectory())
-                      .ConfigureAppConfiguration((builderContext, config) =>
-                      {
-                          var env = builderContext.HostingEnvironment;
-                          config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
-                          config.AddEnvironmentVariables();
-                      });
-                });
-    }
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>().UseContentRoot(Directory.GetCurrentDirectory())
+					  .ConfigureAppConfiguration((builderContext, config) =>
+					  {
+						  var env = builderContext.HostingEnvironment;
+						  config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+								.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+								.AddOcelot(env);
+						  config.AddEnvironmentVariables();
+					  });
+				});
+	}
 }
